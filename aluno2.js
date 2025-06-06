4a8feb7190d72f698a3e73925daf311fd5b66444
Luiz felipe <seuemail@example.com>
2025-06-06 16:01:05 -0300

    Aluno 2 Completo

--- aluno2.js
+++ aluno2.js
@@ -3,4 +3,13 @@ let valores = [a, b, c];
 let i = 0;
 let maior = valores[0];
 
+while(i < valores.length) {
+    if(valores[i] > maior){
+        maior = valores[i];
+    }
+    i++
+}console.log(maior)
+}
 
+
+maiorNumero(5443, 912, 1, 2616281721);
