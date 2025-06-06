30591b97a992acfdcdc460c98ad676fa038e1e06
joao pedro piva <joaopedropiva17@gmail.com>
2025-06-06 16:10:39 -0300

    Implementando a função de soma de array final

--- piva.js
+++ piva.js
@@ -1,9 +1,8 @@
-function Soma(numero1,numero2,numero3,numero4,numero5){
-    const opcoes =[numero1,numero2,numero3,numero4,numero5];
+function Soma(array){
     soma = 0
-    for(let i = 0;i<opcoes.length;i++){
-        soma+= opcoes[i]
+    for(let i = 0;i<array.length;i++){
+        soma+= array[i]
     }
     return soma
 }
-console.log(Soma(20,1,3,4,5))
+console.log(Soma([20,1,3,4,5]))
